301eddfdb2b3f697c0d6fcaf715f55599ffcfeb9
zhyunlong <414228693@qq.com>
2021-02-01 12:07:26 +0800

    first commit

---
+++ dataset.py
@@ -0,0 +1,37 @@
+import torch
+from transformers import *
+
+
+tokenizer = AutoTokenizer.from_pretrained('bert-base-chinese')
+
+def data_collator(features):
+    batch = {}
+    input_ids = [sample["input_ids"] for sample in features]
+    labels = [sample["labels"] for sample in features]
+    batch["input_ids"] = tokenizer(input_ids,  add_special_tokens=True, return_tensors="pt")["input_ids"]
+    batch["labels"] = torch.tensor(labels)
+    return batch
+
+class Mydataset(torch.utils.data.Dataset):
+    def __init__(self, path, label_dict):
+        self.input_seq_all = []
+        self.label_all = []
+        self.label_dict = label_dict
+        with open(path, "r", encoding="utf-8") as f:
+            line = f.readline()
+            while (line != ""):
+                split_line = line.split("\t")
+                assert len(split_line)== 3
+                self.input_seq_all.append(split_line[1])
+                self.label_all.append(split_line[2])
+                line = f.readline()
+
+    def __getitem__(self, idx):
+        sample = dict()
+        sample["input_ids"] = self.input_seq_all[idx]
+        sample["labels"] = self.label_dict[self.label_all[idx]]
+        return sample
+
+    def __len__(self):
+        assert len(self.label_seq_all) == len(self.input_seq_all)
+        return len(self.label_seq_all)
---
+++ main_OCEMOTION.py
@@ -0,0 +1,61 @@
+from dataset import Mydataset,data_collator
+from transformers import *
+
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s' )
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "None"
+
+label_dict = {}
+
+for i in range(17):
+    label_dict["10%d"%i] = i
+train = Mydataset("data/OCEMOTION_train1128.csv", label_dict)
+eval = Mydataset("data/OCEMOTION_train1128.csv", label_dict)
+
+
+label_dict = {0:"O", 1:"，", 2:"。", 3:"：",  4:"、"}
+model = BertForSequenceClassification.from_pretrained("bert-base-chinese")
+training_args = TrainingArguments(
+    output_dir='exp/OCEMOTION/model',          # output directory
+    num_train_epochs=25,              # total # of training epochs
+    per_device_train_batch_size=2,  # batch size per device during training
+    per_device_eval_batch_size=2,   # batch size for evaluation
+    warmup_steps=500,                # number of warmup steps for learning rate scheduler
+    weight_decay=0.01,               # strength of weight decay
+    save_steps=20000,
+    eval_steps=5,
+    logging_dir='exp/OCEMOTION/logs',            # directory for storing logs
+    evaluation_strategy='steps',
+    #prediction_loss_only=True,
+    do_eval=True,
+)
+logging.info(model)
+
+from sklearn.metrics import precision_recall_fscore_support, f1_score, confusion_matrix,classification_report
+
+def compute_metrics(pred):
+    labels = pred.label_ids
+    preds = pred.predictions
+    labels = labels.flatten()
+    preds = preds.flatten()
+    marco_f1_score = f1_score(labels, preds, average='macro')
+    print(marco_f1_score)
+    print(f"{'confusion_matrix':*^80}")
+    print(confusion_matrix(labels, preds, ))
+    print(f"{'classification_report':*^80}")
+    print(classification_report(labels, preds, ))
+
+
+trainer = Trainer(
+    model=model,                         # the instantiated 🤗 Transformers model to be trained
+    args=training_args,                  # training arguments, defined above
+    train_dataset=train,         # training dataset
+    eval_dataset=eval,          # evaluation dataset
+    data_collator= data_collator,
+    compute_metrics=compute_metrics,
+)
+
+
+trainer.train()
+
